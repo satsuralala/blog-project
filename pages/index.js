4f2b8a3e6eda713a9a01231e986fd40d2763ec24
satsuralala <satsuraltumurbat@gmail.com>
2024-07-30 16:29:56 +0800

    7/30

--- pages/index.js
+++ pages/index.js
@@ -4,112 +4,114 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Nav } from "@/components/nav";
 import { Footer } from "@/components/Footer";
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
 const inter = Inter({ subsets: ["latin"] });
-const pageSize=6;
+const pageSize = 6;
 
 export default function All() {
   const [articles, setArticles] = useState([]);
-  const [page, setPage]=useState(1);
-  const [ended, setEnded]=useState(false);
-  const [loading, setLoading]=useState(false);
+  const [page, setPage] = useState(1);
+  const [ended, setEnded] = useState(false);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     loadMore();
   }, []);
 
-  function loadMore(){
+  async function loadMore() {
     setLoading(true);
-      fetch(`https://dev.to/api/articles?username=paul_freeman&page=${page}&per_page=${pageSize}`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((newArticles) => {
-          const updatedArticles=articles.concat(newArticles)
-          setArticles(updatedArticles);
-          setPage(page+1);
-          if(newArticles.length<pageSize){
-            setEnded(true);
-          }
-          setLoading(false);
-        });
 
+    const response = await fetch(
+      `https://dev.to/api/articles?username=paul_freeman&page=${page}&per_page=${pageSize}`
+    );
+    const newArticles = await response.json();
+
+    const updatedArticles = articles.concat(newArticles);
+    setArticles(updatedArticles);
+
+    setPage(page + 1);
+    if (newArticles.length < pageSize) {
+      setEnded(true);
+    }
+    setLoading(false);
   }
-  return (
-    <div className=" bg-white ">
-      <div className="px-20">
-        <Nav></Nav>
-        <h1 className="text-[#181A2A] text-2xl font-bold py-12 pl-4">All Blog Post</h1>
 
-        <div>
-          {articles.map((item,index) => ( index < 5 &&
-              <Link  key={item.id}  href={item.path}>   
-                    <div className="hidden md:flex text-black ">
-                      {item.tag_list[0]}
-                    </div>
-             
-               
-              </Link>    
-          ))}
-        </div>
+return (
+  <div className=" bg-white ">
+    <div className="px-20">
+      <Nav></Nav>
+      <h1 className="text-[#181A2A] text-2xl font-bold py-12 pl-4">
+        All Blog Post
+      </h1>
 
+      <div>
+        {articles.map(
+          (item, index) =>
+            index < 5 && (
+              <Link key={item.id} href={item.path}>
+                <div className="hidden md:flex text-black ">
+                  {item.tag_list[0]}
+                </div>
+              </Link>
+            )
+        )}
+      </div>
 
-
-        <div className="grid md:grid-cols-3 grid-cols-1  gap-5 bg-white mx-4 ">
-          {articles.map((item) => (
-              <Link  key={item.id}  href={item.path}  className=" md:py-4 md:px-4 md:flex md:flex-col md:items-center md:mx-auto border-[#E8E8EA] border-[1px] rounded-xl">   
-                  <Image
-                    src={item.social_image}
-                    width={360}
-                    height={240}
-                    className=" aspect-video object-cover bg-gray-300 rounded-md"
-                  ></Image>
-                  <div className="pt-6 px-4 md:flex-1">
-                    <div className=" badge badge-secondary badge-outline ">
-                      {item.tag_list[0]}
-                    </div>
-                    <div className=" text-2xl  font-semibold text-[#181A2A] pt-4">
-                      {item.title}
-                    </div>
-                    <div className="flex items-center gap-5">
-                      <Image
-                        src={item.user.profile_image_90}
-                        width={36}
-                        height={36}
-                        className="w-[36px] h-[36px]"
-                      ></Image>
-                      <div className="text-[#97989F] text-base font-medium">
-                        {item.user.name}
-                      </div>
-                      <div className="text-[#97989F] text-base font-medium">
-                       {dayjs(item.published_at).fromNow()}
-                      </div>
-                    </div>
-                  </div>
-               
-              </Link>    
-          ))}
-        </div>
-        {!ended&&(<div className="text-center" onClick={loadMore}>
-         
-          <button disabled={loading}  className="btn  my-8 ">
+      <div className="grid md:grid-cols-3 grid-cols-1  gap-5 bg-white mx-4 ">
+        {articles.map((item) => (
+          <Link
+            key={item.id}
+            href={item.path}
+            className=" md:py-4 md:px-4 md:flex md:flex-col md:items-center md:mx-auto border-[#E8E8EA] border-[1px] rounded-xl"
+          >
+            <Image
+              src={item.social_image}
+              width={360}
+              height={240}
+              className=" aspect-video object-cover bg-gray-300 rounded-md"
+            ></Image>
+            <div className="pt-6 px-4 md:flex-1">
+              <div className=" badge badge-secondary badge-outline ">
+                {item.tag_list[0]}
+              </div>
+              <div className=" text-2xl  font-semibold text-[#181A2A] pt-4">
+                {item.title}
+              </div>
+              <div className="flex items-center gap-5">
+                <Image
+                  src={item.user.profile_image_90}
+                  width={36}
+                  height={36}
+                  className="w-[36px] h-[36px]"
+                ></Image>
+                <div className="text-[#97989F] text-base font-medium">
+                  {item.user.name}
+                </div>
+                <div className="text-[#97989F] text-base font-medium">
+                  {dayjs(item.published_at).fromNow()}
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+      {!ended && (
+        <div className="text-center" onClick={loadMore}>
+          <button disabled={loading} className="btn  my-8 ">
             {loading && <span className="loading loading-spinner "></span>}
             loading
           </button>
-        </div>)}
-       
-      </div>
-      <div className="px-20 bg-[#F6F6F7]">
-        <Footer />
-      </div>
-      {/* <filter></Filter>
-      */}
-      
+        </div>
+      )}
     </div>
-    
-  );
-}
+    <div className="px-20 bg-[#F6F6F7]">
+      <Footer />
+    </div>
+    {/* <filter></Filter>
+     */}
+  </div>
+)
+};
