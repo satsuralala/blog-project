158f77576768e2a789ccf5b69c66777dfa9000ee
satsuralala <satsuraltumurbat@gmail.com>
2024-08-09 23:40:51 +0800

    8/9

--- components/nav.js
+++ components/nav.js
@@ -9,9 +9,7 @@ export function Nav(){
                 <Image src={"/Logo.png"} width={158} height={36} className="w-[158px] h-[36px] aspect-auto object-contain"></Image>
             </div>
             <div className=" hidden md:flex gap-10 flex-1">
-                <a className="text-[#3B3C4A] text-base font-normal">Home</a>
-                <a className="text-[#3B3C4A] text-base font-normal" >Blog</a>
-                <a className="text-[#3B3C4A] text-base font-normal">Contact</a>
+                
             </div>
             <div className="form-control pl-16 relative hidden md:block">
                 <input type="text" placeholder="Search" className="bg-[#F4F4F5] rounded-md py-2 px-4 relative "/>
@@ -19,7 +17,7 @@ export function Nav(){
             </div>
             <IoMdMenu className="md:hidden" color="black" size={32} />
   
-            
+            t
 
 </div>
     )
